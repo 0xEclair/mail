4d97a5b63ad41103ec1718f3833ab8ef0049bc6b
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-01-09 01:37:43 +0800

    :feat geist-ui template

--- src/App.js
+++ src/App.js
@@ -1,58 +1,18 @@
 import './App.css';
+import { CssBaseline, GeistProvider } from "@geist-ui/react";
+
+export const Application = () => (
+  <GeistProvider>
+    <CssBaseline />
+    <App />
+  </GeistProvider>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
-export default App;
+export default Application;
