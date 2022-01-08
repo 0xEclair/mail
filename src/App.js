8bf21ab093bbc2dfd3c3740e29fa12994b7e52b0
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-01-09 03:16:09 +0800

    :feat

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { CssBaseline, GeistProvider } from "@geist-ui/react";
+import { CssBaseline, Divider, GeistProvider } from "@geist-ui/react";
 
 export const Application = () => (
   <GeistProvider>
@@ -10,8 +10,27 @@ export const Application = () => (
 
 function App() {
   return (
-    <>
-    </>
+    <div className={"App"}>
+      <div className={"drawer"}>
+        <div className={"drawer-toolbar"}>
+          drawer-toolbar
+        </div>
+        <div className={"drawer-main"}>
+          <Divider h={1} style={{backgroundColor: "#424242"}} />
+          <div>Inbox</div>
+          <Divider h={2} style={{backgroundColor: "#424242"}} />
+          <div>Sent</div>
+          <Divider style={{backgroundColor: "#212121"}} />
+          <div>Logout</div>
+        </div>
+      </div>
+      <div className={"page"}>
+        <div className={"page-toolbar"}>
+          page-toolbar
+        </div>
+        <div className={"page-main"}>page-main</div>
+      </div>
+    </div>
   );
 }
 
