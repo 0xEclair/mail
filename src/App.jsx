8d821db996ff07a747e7e326fd20983036878209
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-01-09 01:21:48 +0800

    :feat page structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
+import theme from "./theme";
+import { Navbar } from "./components/Navbar";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navbar />
+    </ThemeProvider>
+  );
+}
+
+export default App;
